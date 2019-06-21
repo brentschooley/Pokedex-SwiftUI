2ed5a457448bd85c1d9328d300d9660734b09302
Brent Schooley <schooleybrent@gmail.com>
2019-06-21 15:49:08 -0400

    Initial Commit

---
+++ Pokedex/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by Brent Schooley on 6/21/19.
+//  Copyright © 2019 Brent Schooley. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
